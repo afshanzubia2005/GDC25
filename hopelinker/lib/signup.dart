b9c855eab1265fa77c982f03714c69ba0f94c1a0
Scarlet Ceballos <140351738+scarletceballos@users.noreply.github.com>
2025-03-24 14:00:16 -0400

    starting signup screen and created survey and home files

---
+++ hopelinker/lib/signup.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenW = MediaQuery.of(context).size.width;
+    final fieldW = screenW * 0.8;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Account'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  cosnt Text(key: "Join HopeLinker",)
+                ],
+              ))
+          )
+        ))
+    )
+  }
+}
