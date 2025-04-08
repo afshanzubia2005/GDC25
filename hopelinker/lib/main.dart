3df412d684ce685ebd624124f0b64fc4294bfe34
aordillano <134305793+aordillano@users.noreply.github.com>
2025-04-08 10:44:19 -0400

    Firebase auth added, not yet tested

--- hopelinker/lib/main.dart
+++ hopelinker/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hopelinker/signup.dart';
 import 'package:hopelinker/survey.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
