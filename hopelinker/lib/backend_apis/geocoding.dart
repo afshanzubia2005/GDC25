f5d3bb8c89afa5e316fd7b2193cfb0635f143a0a
Afshan <aa3526@njit.edu>
2025-04-23 02:58:21 -0400

    Update geocoding.dart

--- hopelinker/lib/backend_apis/geocoding.dart
+++ hopelinker/lib/backend_apis/geocoding.dart
@@ -4,7 +4,7 @@ import 'dart:convert'; // for jsonDecode
 
 // as of now, this file defines a hardcoded addess value and defines  a function main() that can be used to get its lat/lon coordinates
 
-const addressString = '35 Clark Avenue Edison NJ 08817';
+const addressString = '2013 Deer Ridge Drive, Red Bank NJ 07701';
 
 String buildQuery(String address) {
   final uri_address = Uri.encodeComponent(address);
