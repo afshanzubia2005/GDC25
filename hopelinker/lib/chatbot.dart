8f73f92e64f9300b85ba150df8104b37b119e683
Afshan <goldmine246@gmail.com>
2025-05-26 17:58:44 -0400

    chatbot

--- hopelinker/lib/chatbot.dart
+++ hopelinker/lib/chatbot.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav_bar.dart';
 
-class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+class ChatbotPage extends StatelessWidget {
+  const ChatbotPage({super.key});
 
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chat Bot')),
-      body: Center(child: Text('here is the chatbot!! ')),
+      appBar: AppBar(
+        toolbarHeight: 60,
+        automaticallyImplyLeading: false,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.orange.shade200, Colors.yellow.shade100],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: const Text(
+          'Chatbot',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Chatbot Page', style: TextStyle(fontSize: 24)),
+      ),
+      bottomNavigationBar: const CustomBottomNavBar(selectedIndex: 2),
     );
   }
 }
